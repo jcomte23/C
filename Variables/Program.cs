7db69bc3c83614755e9a4013fdf4318446a27234
Javier Combita Tellez <javiercombita2014@gmail.com>
2022-10-28 16:31:54 -0500

    variables tipo Int y parseo de int a string

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -6,9 +6,17 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            int currentYear = 2022;
+            int yearOfBirth; 
+
             Console.WriteLine("What is your name? Please write your full name");
             var fullName=Console.ReadLine();
-            Console.WriteLine("hello "+fullName+", Welcome ,this is my first program");
+            Console.WriteLine("Welcome " + fullName+", this is my first program");
+            Console.WriteLine("how old are you?");
+            int age=Convert.ToInt32(Console.ReadLine());
+            yearOfBirth=currentYear-age;
+            Console.WriteLine("you were born in " + yearOfBirth);
+
         }
     }
 }
