a0711cf55b475237ffda188ec4b564e4a5033606
Javier Combita Tellez <javiercombita2014@gmail.com>
2022-10-28 14:35:20 -0500

    manejo de strings en C#

---
+++ Variables/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name? Please write your full name");
+            var fullName=Console.ReadLine();
+            Console.WriteLine("hello "+fullName+", Welcome ,this is my first program");
+        }
+    }
+}
