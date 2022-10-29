54c1ce44f9b8c2e618d69de042d5dea5e2fa4771
Javier Combita Tellez <javiercombita2014@gmail.com>
2022-10-29 16:19:38 -0500

    metodos predefinidos de String en C#

---
+++ MetodosStringPredefinidos/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Metodos String");
+
+string mensajeString = "Javier Cómbita Tellez";
+string segundoMensaje = "Javier Cómbita Tellez";
+
+/*clonar un string
+string mensajeClonado = mensajeString.Clone().ToString();
+Console.WriteLine(mensajeClonado);*/
+
+/*compara 2 strings
+var resultado = mensajeString.CompareTo(segundoMensaje);
+Console.WriteLine(resultado);*/
+
+/*retorna TRUE o False dependiendo de si encuentra o no encuentra el parametro
+ * dentro del string
+var resultado = mensajeString.Contains("Tellez");
+Console.WriteLine(resultado);*/
+
+
+Console.ReadKey();
