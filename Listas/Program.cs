de657f2bcd4441808da64fa7abf3dcf1f5f38c92
Javier Combita Tellez <javiercombita2014@gmail.com>
2022-10-29 14:34:02 -0500

    listas en C#

---
+++ Listas/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Listas en C#");
+
+Console.WriteLine("Lista de utiles escolares\n");
+
+List<string> utilesEscolares = new List<string>();
+
+utilesEscolares.Add("cuaderno");
+utilesEscolares.Add("lapiz");
+utilesEscolares.Add("borrador");
+utilesEscolares.Add("colores");
+utilesEscolares.Add("regla");
+
+foreach (var item in utilesEscolares)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("\nLista sin los colores\n");
+utilesEscolares.Remove("colores");
+foreach (var item in utilesEscolares)
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadKey();
